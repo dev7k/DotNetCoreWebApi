3c5f71530793dc4fe93ec49b165315dd017da8aa
dev7k <dev7k@protonmail.com>
2019-03-22 12:13:17 +0100

    added measurement context

---
+++ src/WebApiServer/Database/MeasurementContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiServer.Models;
+
+namespace WebApiServer.Database
+{
+    public class MeasurementContext : DbContext
+    {
+        public MeasurementContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Measurement> Measurements { get; set; }
+    }
+}
